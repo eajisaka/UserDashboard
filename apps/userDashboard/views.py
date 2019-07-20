31433f8737f697017cb50b9c6e8e34eb268eba42
Eric <eajisaka@gmail.com>
2019-07-20 15:30:40 -0700

    added superadmin level

--- apps/userDashboard/views.py
+++ apps/userDashboard/views.py
@@ -19,7 +19,7 @@ def register_user(request):
     users = User.objects.all()
     admin = 0
     if len(users) == 0:
-        admin = "admin"
+        admin = "superadmin"
     else:
         admin = "normal"
 
@@ -45,7 +45,7 @@ def signin_user(request):
     request.session['loggedin'] = True
     user = User.objects.get(email=request.POST['sign_in_email'])
     request.session['user_id'] = user.id
-    if user.admin_level == "admin":
+    if user.admin_level == "superadmin" or user.admin_level =="admin":
         return redirect('/dashboard/admin')
     return redirect('/dashboard')
 
@@ -62,25 +62,19 @@ def admin_dashboard(request):
     if request.session['user_id'] == None:
         return redirect('/')
     user = User.objects.get(id = request.session['user_id'])
-    if user.admin_level != "admin": 
-        return redirect('/dashboard')
-    users = User.objects.all()
-    context = {
-        "users" : users
-    }
-    return render(request, "userDashboard/admin_dashboard.html", context)
+    if user.admin_level == "superadmin" or user.admin_level == "admin": 
+        users = User.objects.all()
+        context = {
+        "users" : users,
+        "user" : user
+        }
+        return render(request, "userDashboard/admin_dashboard.html", context)
+    return redirect('/dashboard')
 
 def logout(request):
     request.session['loggedin'] = False
     request.session['user_id'] = None
     return redirect('/')
-
-def delete_user(request, user_id):
-    if request.session['user_id'] == None:
-        return redirect('/')
-    remove_user = User.objects.get(id = user_id)
-    remove_user.delete()
-    return redirect('/dashboard/admin')
 
 def message(request, user_id):
     print("now in message view!")
@@ -152,11 +146,31 @@ def edit_description(request):
     return redirect('/edit')
 
 def admin_edit_profile(request, user_id):
-    user = User.objects.get(id = user_id)
+    curr_user = User.objects.get(id=request.session['user_id'])
+    profile_user = User.objects.get(id = user_id)
     context = {
-        'user': user
+        'user': profile_user,
+        'curr_user' : curr_user
     }
     return render(request, "userDashboard/admin_edit_profile.html", context)
+
+def delete_user(request, user_id):
+    if request.session['user_id'] == None:
+        return redirect('/')
+    curr_user = User.objects.get(id =request.session['user_id'])
+    remove_user = User.objects.get(id = user_id)
+    if remove_user.id == 1 and remove_user.admin_level=="superadmin":
+        messages.error(request, "You can't delete the head honcho superadmin!", extra_tags = "delete_error")
+        return redirect('/dashboard/admin')
+    if curr_user.admin_level != "superadmin":
+        messages.error(request, "You can't delete another admin!", extra_tags = "delete_error")
+        return redirect('/dashboard/admin')
+    if remove_user.admin_level == "superadmin":
+            messages.error(request, "You can't delete another superadmin!", extra_tags = "delete_error")
+            return redirect('/dashboard/admin')
+        
+    remove_user.delete()
+    return redirect('/dashboard/admin')
 
 def admin_edit_password(request, user_id):
     errors = User.objects.validator_change_password(request.POST)
@@ -164,7 +178,18 @@ def admin_edit_password(request, user_id):
         for key, value in errors.items():
             messages.error(request, value, extra_tags = key)
         return redirect(f'/edit/profile/{user_id}')
+    curr_user = User.objects.get(id =request.session['user_id'])
     user_update = User.objects.get(id = user_id)
+    if user_update.admin_level == "superadmin" or user_update.admin_level =="admin":
+        if curr_user.admin_level =="superadmin" and curr_user.id == 1:
+            hash1 = bcrypt.hashpw(request.POST['edit_pass1'].encode(), bcrypt.gensalt())
+            user_update.pw_hash = hash1
+            user_update.save()
+            messages.success(request, 'Password updated!', extra_tags = 'password')
+            return redirect(f'/edit/profile/{user_id}')
+        if curr_user.admin_level != "superadmin" and user_update.id != request.session['user_id']:
+            messages.error(request, "You can't change other admins' passwords!", extra_tags = 'edit_pass_error')
+            return redirect(f'/edit/profile/{user_id}')
     hash1 = bcrypt.hashpw(request.POST['edit_pass1'].encode(), bcrypt.gensalt())
     user_update.pw_hash = hash1
     user_update.save()
@@ -172,13 +197,25 @@ def admin_edit_password(request, user_id):
     return redirect(f'/edit/profile/{user_id}')
 
 def admin_edit_user(request, user_id):
-    print(int(request.POST['edit_id']) + 5)
     errors = User.objects.validator_profile(request.POST)
     if len(errors) > 0:
         for key, value in errors.items():
             messages.error(request, value, extra_tags = key)
         return redirect(f'/edit/profile/{user_id}')
+    curr_user = User.objects.get(id =request.session['user_id'])
     user_update = User.objects.get(id=user_id)
+    if user_update.admin_level == "superadmin" or user_update.admin_level =="admin":
+        if curr_user.admin_level =="superadmin" and curr_user.id == 1:
+            user_update.email = request.POST['edit_email']
+            user_update.first_name = request.POST['edit_first_name']
+            user_update.last_name = request.POST['edit_last_name']
+            user_update.admin_level = request.POST['edit_user_level']
+            user_update.save()
+            messages.success(request, 'Profile updated!', extra_tags = 'profile')
+            return redirect(f'/edit/profile/{user_id}')
+        if curr_user.admin_level != "superadmin" or user_update.id != request.session['user_id']:
+            messages.error(request, "You can't change other admins' information!", extra_tags = 'edit_error')
+            return redirect(f'/edit/profile/{user_id}')
     user_update.email = request.POST['edit_email']
     user_update.first_name = request.POST['edit_first_name']
     user_update.last_name = request.POST['edit_last_name']
